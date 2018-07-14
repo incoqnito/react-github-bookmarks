72f01412c217c59c9fec93945b4f8c5d62bd54ea
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2018-07-14 11:23:30 +0200

    persist state in local storage

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,17 @@ import { createStore } from 'redux'
 import { middleware } from './middleware'
 import { reducer } from './reducer'
 
-export const store = createStore(reducer, middleware)
+import { loadState, saveState } from './localStorage'
+import throttle from 'lodash/throttle'
+
+const persitedState =loadState();
+
+export const store = createStore(reducer, persitedState, middleware)
+
+store.subscribe(throttle(() => {
+  saveState({
+    BookmarkReducer: store.getState().BookmarkReducer
+  });
+}, 1000));
 
 window.store = store
